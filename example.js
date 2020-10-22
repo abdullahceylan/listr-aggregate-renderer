5bc67d91c059b492765231cba4f521277831d484
Abdullah Ceylan <abdullah@creativouse.co.uk>
2020-10-23 00:40:42 +0100

    Initial commit

---
+++ example.js
@@ -0,0 +1,123 @@
+'use strict';
+const Observable = require('zen-observable');
+const logSymbols = require('log-symbols');
+const delay = require('delay');
+const Listr = require('listr');
+const renderer = require('./');
+
+const tasks = new Listr([
+	{
+		title: 'Git',
+		task: () => {
+			return new Listr([
+				{
+					title: 'Checking git status',
+					task: () => {
+						return new Observable(observer => {
+							observer.next('foo');
+
+							delay(2000)
+								.then(() => {
+									observer.next('bar');
+									return delay(2000);
+								})
+								.then(() => {
+									observer.next(0);
+									return delay(500);
+								})
+								.then(() => {
+									observer.next(1);
+									return delay(500);
+								})
+								.then(() => {
+									observer.next({hello: 'world'});
+									return delay(500);
+								})
+								.then(() => {
+									observer.next('');
+									return delay(500);
+								})
+								.then(() => {
+									observer.complete();
+								});
+						});
+					}
+				},
+				{
+					title: 'Checking remote history',
+					task: () => delay(2000)
+				}
+			], {concurrent: true});
+		}
+	},
+	{
+		title: 'Install npm dependencies',
+		task: () => delay(2000)
+	},
+	{
+		title: 'Counting to 7',
+		task: () => {
+			return new Listr([
+				{
+					title: '1',
+					task: () => delay(2000)
+				},
+				{
+					title: '2',
+					task: () => delay(3800)
+				},
+				{
+					title: '3',
+					task: () => delay(3800)
+				},
+				{
+					title: '4',
+					task: () => delay(4000)
+				},
+				{
+					title: '5',
+					task: () => delay(5500)
+				},
+				{
+					title: '6',
+					task: () => delay(6000)
+				},
+				{
+					title: '7',
+					task: () => delay(7000)
+				},
+			], {concurrent: true});
+		}
+	},
+	{
+		title: 'Run tests',
+		task: () => delay(2000).then(() => {
+			return new Observable(observer => {
+				observer.next('clinton && xo && ava');
+
+				delay(2000)
+					.then(() => {
+						observer.next(`${logSymbols.success} 7 passed`);
+						return delay(2000);
+					})
+					.then(() => {
+						observer.complete();
+					});
+			});
+		})
+	},
+	{
+		title: 'Publish package',
+		task: () => delay(1000).then(() => {
+			throw new Error('Package name already exists');
+		})
+	}
+], {
+	renderer,
+	aggregate: true,
+	maxsubtasks: 2,
+});
+
+tasks.run().catch(err => {
+	console.error(err.message);
+});
